fc0a730febd42633c67924c7bbff649eaa9b2cdf
Ashley Kucuk <ashleyskucuk@gmail.com>
2023-03-27 18:02:37 -0400

    assigned feature file to me

---
+++ src/test/java/com/library/steps/us03_step_definitions_AK.java
@@ -0,0 +1,34 @@
+package com.library.steps;
+
+import io.cucumber.java.en.*;
+
+public class us03_step_definitions_AK {
+
+
+
+    @Given("the {string} on the home page")
+    public void the_on_the_home_page(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the user navigates to {string} page")
+    public void the_user_navigates_to_page(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("the user clicks book categories")
+    public void the_user_clicks_book_categories() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("verify book categories must match book_categories table from db")
+    public void verify_book_categories_must_match_book_categories_table_from_db() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
+
+
+}
