a06b1f7dd4f5987469288d3ceeeb882dcca198ef
Ashley Kucuk <ashleyskucuk@gmail.com>
2023-03-27 18:52:23 -0400

    new methods for SD

--- src/test/java/com/library/runner/CukesRunner.java
+++ src/test/java/com/library/runner/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features" ,
         glue = "com/library/steps",
         dryRun = true,
-        tags = "@wip"
+        tags = "@wip" + "db"
 )
 public class CukesRunner {
 
--- src/test/java/com/library/steps/us03_step_definitions_AK.java
+++ src/test/java/com/library/steps/us03_step_definitions_AK.java
@@ -1,20 +1,25 @@
 package com.library.steps;
 
+import com.library.pages.BasePage;
+import com.library.pages.BookPage;
+import com.library.pages.LoginPage;
 import io.cucumber.java.en.*;
 
-public class us03_step_definitions_AK {
+public class us03_step_definitions_AK  {
 
+LoginPage loginPage = new LoginPage();
+
+    BookPage bookPage = new BookPage();
 
 
     @Given("the {string} on the home page")
-    public void the_on_the_home_page_AK(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void the_on_the_home_page_AK(String username) {
+        loginPage.login(username);
     }
     @When("the user navigates to {string} page")
-    public void the_user_navigates_to_page_AK(String string) {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    public void the_user_navigates_to_page_AK(String module) {
+    bookPage.navigateModule(module);
+
     }
     @When("the user clicks book categories")
     public void the_user_clicks_book_categories_AK() {
