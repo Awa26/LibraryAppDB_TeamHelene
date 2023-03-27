b36bd78395c0bd81d4a1da625ebf60ac9194fa68
Ashley Kucuk <ashleyskucuk@gmail.com>
2023-03-27 18:23:45 -0400

    assigning steps_defs to me

--- src/test/java/com/library/steps/us03_step_definitions_AK.java
+++ src/test/java/com/library/steps/us03_step_definitions_AK.java
@@ -7,22 +7,22 @@ public class us03_step_definitions_AK {
 
 
     @Given("the {string} on the home page")
-    public void the_on_the_home_page(String string) {
+    public void the_on_the_home_page_AK(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
     @When("the user navigates to {string} page")
-    public void the_user_navigates_to_page(String string) {
+    public void the_user_navigates_to_page_AK(String string) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
     @When("the user clicks book categories")
-    public void the_user_clicks_book_categories() {
+    public void the_user_clicks_book_categories_AK() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
     @Then("verify book categories must match book_categories table from db")
-    public void verify_book_categories_must_match_book_categories_table_from_db() {
+    public void verify_book_categories_must_match_book_categories_table_from_db_AK() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
