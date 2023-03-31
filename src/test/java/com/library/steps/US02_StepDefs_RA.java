4a1ca1705033b1e0354b8cc4260b42864ca6efbc
rusulalhafidh <rossul.alhafidh@gmail.com>
2023-03-31 01:40:34 -0400

    Updated US02

--- src/test/java/com/library/steps/US02_StepDefs_RA.java
+++ src/test/java/com/library/steps/US02_StepDefs_RA.java
@@ -16,16 +16,16 @@ public class US02_StepDefs_RA {
         DashBoardPage dashBoardPage;
         String actualBorrowedBooksNumber;
 
-        @Given("the {string} on the home page")
-        public void the_on_the_home_page(String user) {
+        @Given("the {string} on the home page RA")
+        public void the_on_the_home_page_RA(String user) {
             loginPage=new LoginPage();
             loginPage.login(user);
             BrowserUtil.waitFor(2);
 
         }
 
-        @When("the librarian gets borrowed books number")
-        public void the_librarian_gets_borrowed_books_number() {
+        @When("the librarian gets borrowed books number RA")
+        public void the_librarian_gets_borrowed_books_number_RA() {
             dashBoardPage=new DashBoardPage();
 
             actualBorrowedBooksNumber = dashBoardPage.borrowedBooksNumber.getText();
@@ -35,8 +35,8 @@ public class US02_StepDefs_RA {
 
 
         }
-        @Then("borrowed books number information must match with DB")
-        public void borrowed_books_number_information_must_match_with_db() {
+        @Then("borrowed books number information must match with DB RA")
+        public void borrowed_books_number_information_must_match_with_db_RA() {
 
             String query="select count(*) from book_borrow where is_returned=0";
 
